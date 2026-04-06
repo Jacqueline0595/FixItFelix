a37a5dacc960e9cfc0139fa70d858494f99da551
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-04-05 18:00:35 -0600

    Ya se subio

--- src/main/java/com/idk/fixitfelix/game/Game.java
+++ src/main/java/com/idk/fixitfelix/game/Game.java
@@ -4,15 +4,13 @@ import com.idk.fixitfelix.entities.Entity;
 import javafx.scene.layout.Pane;
 
 public class Game {
-    private Entity felix;
-    private Pane root;
+    private final Entity felix;
 
     public Game(Pane root)
     {
-        this.root = root;
         felix = new Entity("/sprites/Felix.png", 100, 100);
 
-        root.getChildren().add(felix.getSprite()); // ola
+        root.getChildren().add(felix.getSprite());
     }
 
     public void update()
