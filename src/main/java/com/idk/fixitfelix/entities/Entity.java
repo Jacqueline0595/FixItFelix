8593d8e50417e36f6ff1d0fd17c3f51a5d06525a
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-04-12 00:04:40 -0600

    Se ve una animacion y felix camina bien a una velocidad adecuada

--- src/main/java/com/idk/fixitfelix/entities/Entity.java
+++ src/main/java/com/idk/fixitfelix/entities/Entity.java
@@ -2,10 +2,22 @@ package com.idk.fixitfelix.entities;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.geometry.Rectangle2D;
 
 import java.util.Objects;
 
 public class Entity {
+    protected int frame = 0;
+    protected int currentRow = 0;
+
+    protected double offsetY = 30;
+    protected double frameWidth = 40;
+    protected double frameHeight = 70;
+    protected int maxFrames = 4;
+
+    protected double frameTimer = 0;
+    protected double frameDelay = 25; // velocidad (más bajo = más rápido)
+
     protected double x;
     protected double y;
 
@@ -35,8 +47,45 @@ public class Entity {
     public void update()
     {
         move();
+
+        updateAnimation();
+
         sprite.setX(x);
         sprite.setY(y);
+    }
+
+    protected void updateAnimation()
+    {
+        frameTimer++;
+
+        if (frameTimer >= frameDelay)
+        {
+            frame++;
+            frameTimer = 0;
+
+            if (frame >= maxFrames)
+            {
+                frame = 0;
+            }
+        }
+
+        sprite.setViewport(new Rectangle2D(
+                30 + frame * 100,
+                currentRow * 100 + offsetY,
+                frameWidth,
+                frameHeight
+        ));
+    }
+
+    public void setAnimation(int row, double offsetY, double width, double height, int frames)
+    {
+        this.currentRow = row;
+        this.offsetY = offsetY;
+        this.frameWidth = width;
+        this.frameHeight = height;
+        this.maxFrames = frames;
+
+        this.frame = 0; // reinicia animación
     }
 
     public ImageView getSprite()
