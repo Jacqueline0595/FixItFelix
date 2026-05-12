3658789eff5eaf3c8632fc472c94a4eb161a4969
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-05-12 14:37:45 -0600

    Animacion inicial

--- src/main/java/com/idk/fixitfelix/HelloApplication.java
+++ src/main/java/com/idk/fixitfelix/HelloApplication.java
@@ -1,34 +1,77 @@
 package com.idk.fixitfelix;
 
-import com.idk.fixitfelix.game.Game;
-import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Objects;
 
 public class HelloApplication extends Application {
 
-    private Game game;
+    private boolean mostrar = true;
+
     @Override
     public void start(Stage stage) {
+        // Contenedor vacio
+        Pane root = new Pane();
 
-        Pane root = new Pane(); // contenedor vacio
+        // Carga de las dos imagenes para animarlas
+        Image inicio1 = new Image( Objects.requireNonNull( getClass().getResourceAsStream("/LoopInicio/1.png" ) ) );
+        Image inicio2 = new Image( Objects.requireNonNull( getClass().getResourceAsStream("/LoopInicio/2.png" ) ) );
 
-        game = new Game(root);
+        // Para que se vean las imagenes
+        ImageView img1 = new ImageView(inicio1);
+        ImageView img2 = new ImageView(inicio2);
 
-        Scene scene = new Scene(root, 680, 520);
+        img1.setLayoutX(0);
+        img1.setLayoutY(0);
+
+        img2.setLayoutX(0);
+        img2.setLayoutY(0);
+
+        root.getChildren().addAll(img1, img2);
+
+        Scene scene = getScene(root, img1, img2);
+
         stage.setTitle("Fix It Felix!");
         stage.setScene(scene);
         stage.show();
+    }
 
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                game.update();
+    private Scene getScene(Pane root, ImageView img1, ImageView img2) {
+        Scene scene = new Scene(root, 680, 520);
+
+        // Se encarga de alternar las imagenes
+        Timeline animacion = new Timeline(
+                new KeyFrame(Duration.millis(500), e -> {
+                    mostrar = !mostrar;
+                    img1.setVisible(mostrar);
+                    img2.setVisible(!mostrar);
+                } )
+        );
+
+        animacion.setCycleCount(Timeline.INDEFINITE);
+        animacion.play();
+
+        // Detecta si se presiono espacio o enter
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.SPACE || e.getCode() == KeyCode.ENTER) {
+                // Detiene la animacion
+                animacion.stop();
+                // Quita las imagenes
+                root.getChildren().removeAll(img1, img2);
+
+                // Aviso
+                System.out.println("Espacio o enter");
             }
-        };
-
-        timer.start();
+        });
+        return scene;
     }
 }
